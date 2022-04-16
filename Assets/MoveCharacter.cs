f78075ff1a96a67730624d8f27f3177cb3069d39
Michael Young <coyote38@rams.colostate.edu>
2022-04-16 17:41:36 -0600

    implemented limited jump

--- Assets/MoveCharacter.cs
+++ Assets/MoveCharacter.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class MoveCharacter : MonoBehaviour
 {
     public float walkSpeed;
+	public float airSpeed;
 	public float jumpSpeed;
 	public bool onGround;
 	public Vector2 jump;
 	public Rigidbody2D rb2D;
 	
 	void Start() {
+		walkSpeed = 5f;
+		airSpeed = 6.5f;
+		jumpSpeed = 300f;
 		jump = new Vector2(0,1);
 		rb2D = GetComponent<Rigidbody2D>();
 		onGround = true;
@@ -25,10 +29,16 @@ public class MoveCharacter : MonoBehaviour
 			rb2D.AddForce(jump * jumpSpeed);
 			onGround = false;
 		}
-		if (Input.GetKey("a")) {
+		if (Input.GetKey("a") && !onGround) {
+			pos.x -= airSpeed * Time.deltaTime;
+		}
+		else if (Input.GetKey("a")) {
 			pos.x -= walkSpeed * Time.deltaTime;
 		}
-		if (Input.GetKey("d")) {
+		if (Input.GetKey("d") && !onGround) {
+			pos.x += airSpeed * Time.deltaTime;
+		}
+		else if (Input.GetKey("d")) {
 			pos.x += walkSpeed * Time.deltaTime;
 		}
 		
